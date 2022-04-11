baf169029f236b9b270a0c02180756adc1baf611
Zican Yang <zicanyang@outlook.com>
2022-04-11 16:23:05 -0400

    args

---
+++ Makefile
@@ -0,0 +1,18 @@
+APP_NAME=influence
+
+OBJS=main.o
+
+CXX = g++ -m64 -std=c++11
+# CXXFLAGS = -I. -O0 -g -Wall -fopenmp -Wno-unknown-pragmas
+CXXFLAGS = -I. -O3 -Wall -fopenmp -Wno-unknown-pragmas
+
+default: $(APP_NAME)
+
+$(APP_NAME): $(OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $(OBJS)
+
+%.o: %.cpp
+	$(CXX) $< $(CXXFLAGS) -c -o $@
+
+clean:
+	/bin/rm -rf *~ *.o $(APP_NAME) *.class
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    double startTime;
+    double endTime;
+    int nSeeds = 10;
+    int nMonteCarloSimulations = 100;
+    double prob = 0.1;
+    char *inputFilename = NULL;
+    int opt = 0;
+
+    // Read command line arguments
+    do {
+        opt = getopt(argc, argv, "f:p:i:s:");
+        switch (opt) {
+        case 'f':
+            inputFilename = optarg;
+            break;
+
+        case 'p':
+            prob = atof(optarg);
+            break;
+
+        case 'i':
+            nMonteCarloSimulations = atoi(optarg);
+            break;
+
+        case 's':
+            nSeeds = atoi(optarg);
+            break;
+
+        case -1:
+            break;
+
+        default:
+            break;
+        }
+    } while (opt != -1);
+
+    if (inputFilename == NULL) {
+        printf("Usage: %s -f <filename> [-p <P>] [-i <N_iters>]\n", argv[0]);
+        return -1;
+    }
+
+    // Run computation
+    // compute(procID, nproc, inputFilename, prob, numIterations, &startTime, &endTime);
+
+    // Cleanup
+    // printf("Elapsed time for proc %d: %f\n", procID, endTime - startTime);
+    return 0;
+}
